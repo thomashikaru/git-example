61088f09e1b5a4818093c95860756710c37aba52
Thomas Clark <thomas.clark@vimeo.com>
2021-08-01 22:08:29 +0100

    commiting new file example.py

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # git-example
+
+A repository to demonstrate the usage of git and GitHub
---
+++ example.py
@@ -0,0 +1,9 @@
+# example Python file
+
+
+def example_function(n):
+    return n * n
+
+
+if __name__ == "__main__":
+    print(example_function(5))
