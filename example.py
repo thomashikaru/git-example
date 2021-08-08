4353639951326bb668adc0e1f17bfa470afd2a2f
Clark <clark@Clarks-Air.lan>
2021-08-08 15:26:21 -0400

    added graphing functionality

--- example.py
+++ example.py
@@ -1,9 +1,20 @@
 # example Python file
-
+import numpy
+import matplotlib.pyplot as plt
 
 def example_function(n):
-    return n * n
+    return n**2
 
+def graphing_function(x):
+    y = example_function(x)
+    
+    plt.style.use("seaborn")
+    fig, ax = plt.subplot()
+    ax.plot(x, y)
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    plt.show()
 
 if __name__ == "__main__":
-    print(example_function(5))
+    print(example_function(np.array(10)))
+    graphing_function(np.linspace(0, 10))
